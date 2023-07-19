d3c72967ff1391eb4c70cfb28f3eaf9ac0fff98c
Praba31-1993 <prabaidymoto@gmail.com>
2023-07-19 19:16:35 +0530

    sql query

--- SQLQuery_2023.07.19.sql
+++ SQLQuery_2023.07.19.sql
@@ -16,9 +16,27 @@ Create table Admin
 
 )
 
-insert into Admin (Name, MailID, IsActive, Created_at) Values ('Praba', 'praba@gmail.com',1,CURRENT_TIMESTAMP),('karthik', 'karthik@gmail.com',1, CURRENT_TIMESTAMP),('Naren', 'naren@gmail.com',1, CURRENT_TIMESTAMP)
 
-Select * from Admin
+Create table Roles
+(
+roleid int identity(1,1) primary key,
+role varchar(25) unique,
+Created_at DATETIME DEFAULT CURRENT_TIMESTAMP ,
+)
+
+INSERT INTO Roles (role)
+VALUES
+    ('MD'),
+    ('CEO'),
+    ('CTO'),
+    ('SE'),
+    ('HR'),
+    ('PM'),
+    ('TL'),
+    ('TM'),
+    ('TESTER');
+
+Select * from Roles
 Drop table Admin
 Drop Database Twilight
 
@@ -31,32 +49,22 @@ LastName varchar(50),
 Email varchar(360) unique,
 MobileNumber varchar(15) unique,
 Address varchar(500) Not Null,
-Designation varchar(100) Not Null,
+DOJ  datetime Not Null,
 Experience int Not Null,
-ID int,
+roleId int,
 Gender varchar(10),
 isActive bit,
 SalaryID int,
-Foreign key(ID) References Admin(ID),
+Foreign key(roleId) References Role(roleid),
 Foreign key(SalaryID) References Salaries(SalaryID),
 Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 Modified_date DATETIME
 )
 
-INSERT INTO Employee (FirstName, LastName, Email, MobileNumber, Address, Designation, Experience, ID, Gender, isActive, SalaryID, Created_at, Modified_date)
-VALUES
-  ('John', 'Doe', 'johndoe@example.com', '1234567890', '123 Main St', 'FrontEnd Developer', 3, 1, 'Male', 1, 1, CURRENT_TIMESTAMP, NULL),
-  
-  ('Jane', 'Smith', 'janesmith@example.com', '9876543210', '456 Elm St', 'BackEnd Developer', 2, 2, 'Female', 1, 2, CURRENT_TIMESTAMP, NULL),
-  
-  ('Mike', 'Johnson', 'mikejohnson@example.com', '5551112222', '789 Oak St', 'FrontEnd Developer', 5, 3, 'Male', 1, 3, CURRENT_TIMESTAMP, NULL),
-  
-  ('Emily', 'Brown', 'emilybrown@example.com', '4445556666', '987 Maple Ave', 'Architect', 4, 1, 'Female', 1, 5, CURRENT_TIMESTAMP, NULL),
-  
-  ('David', 'Wilson', 'davidwilson@example.com', '7778889999', '321 Pine St', 'Full Stack Developer', 1, 2, 'Male', 1, 3, CURRENT_TIMESTAMP, NULL);
 
 
-Create table Salaries
+
+Create table Salary
 (
 SalaryID int identity(1,1) primary key,
 CTC varchar(10) Not Null,
@@ -66,10 +74,12 @@ DA varchar(10) Not Null,
 HRA varchar(10) Not Null,
 Medical varchar(10) Not Null,
 ConvenientAllowance varchar(10),
+Special varchar(10),
 Bonus varchar(10),
-NetPay varchar(10) Not Null,
-ID int,
-Foreign key(ID) References Admin(ID),
+Increment varchar(10) Not Null,
+Hike varchar(10) Not Null,
+roleId int,
+Foreign key(roleId) References Role(roleid),
 Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 Modified_date DATETIME
 )
@@ -112,11 +122,11 @@ Select * from Admin
 Select * from Salaries
 Select * from Employee
 
-CREATE PROCEDURE SoftDeleteRecord(@aid int,@RecordID int)
+CREATE PROCEDURE SoftDeleteRecords(@aid int,@RecordID int)
    
 AS
 BEGIN
-    UPDATE Employee SET isActive = 0,ID=@aid,  Modified_date=getDate() WHERE EmpId = @RecordID;
+    UPDATE Employee SET isActive = 0,roleid=@aid,  Modified_date=getDate() WHERE EmpId = @RecordID;
 END
 
 
@@ -181,6 +191,8 @@ Select Employee.FirstName, Employee.LastName, Employee.EmpId, Employee.Email, Em
 from Employee
 full outer join Salaries
 ON Employee.SalaryID = Salaries.SalaryID
+
+/*For the Year, Employees how much salary he got for every month wise */
 
 
 
