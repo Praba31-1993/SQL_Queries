215378e2098f3e848424f75773105bfa1c890896
Praba31-1993 <prabaidymoto@gmail.com>
2023-07-24 20:45:45 +0530

    function

--- SQLQuery_2023.07.24.sql
+++ SQLQuery_2023.07.24.sql
@@ -20,26 +20,33 @@ Update Employees SET Experience =5 Where Eid=2
 Give input as 'praba123Welcome2Tescom'. But My Output is prabaWelcomeTescom using Function
 */
 
-DECLARE @FirstName varchar(100)
-SET @FirstName = 'praba123W5555elcome2Tescom'
+CREATE FUNCTION FN_AlphabeticValues (@Name varchar(100))
+RETURNS varchar(100)
+AS   
+BEGIN  
+    WHILE PATINDEX('%[0-9]%', @Name) > 0
+        SET @Name = REPLACE(@Name, SUBSTRING(@Name, PATINDEX('%[0-9]%', @Name), 1), '')
 
+    WHILE PATINDEX('%[^a-zA-Z]%', @Name) > 0
+        SET @Name = REPLACE(@Name, SUBSTRING(@Name, PATINDEX('%[^a-zA-Z]%', @Name), 1), '')
 
-WHILE PATINDEX('%[0-9]%', @FirstName) > 0
-    SET @FirstName = REPLACE(@FirstName, SUBSTRING(@FirstName, PATINDEX('%[0-9]%', @FirstName), 1), '')
+    RETURN @Name;
+END;
 
+SELECT dbo.FN_AlphabeticValues('praba123Welcome+2Tescom') AS Name;
 
-WHILE PATINDEX('%[^a-zA-Z]%', @FirstName) > 0
-    SET @FirstName = REPLACE(@FirstName, SUBSTRING(@FirstName, PATINDEX('%[^a-zA-Z]%', @FirstName), 1), '')
-
-SELECT @FirstName
 
 /*Task 2 
 Give input as '2023-07-20 13:03:49 687'. But My Output is '2023-07-20' using Function
 */
 
-Select Convert(date, DOJ) as DOJ FROM Employees
+CREATE FUNCTION FN_Dates(@date datetime)
+RETURNS date
+AS   
+BEGIN  
+    RETURN Convert(date, @date) 
+END;   
 
-
-
+SELECT dbo.FN_Dates('2025-05-10 18:20:30') AS Date;
 
 
